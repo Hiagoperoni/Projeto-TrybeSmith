d234a444acdf7ff8fdd5cb8556b3edefbe63cff1
HiagoP <hiagoperonip@gmail.com>
2023-02-07 18:01:36 -0300

    Requisito 06 pronto

--- src/Routes/productRoutes.ts
+++ src/Routes/productRoutes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import ProductController from '../controllers/productController';
+import productNameMidd from '../Middlewares/productNameMidd';
+import productAmountMidd from '../Middlewares/productAmountMidd';
 
 const productController = new ProductController();
 const productRouter = Router();
 
 productRouter.get('/', (req, res) => productController.getAll(req, res));
-productRouter.post('/', (req, res) => productController.createProduct(req, res));
+productRouter.post(
+  '/', 
+  productNameMidd,
+
+  productAmountMidd,
+
+  (req, res) => productController.createProduct(req, res),
+);
 
 export default productRouter;
